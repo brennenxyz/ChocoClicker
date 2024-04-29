938e8f5f19fa8dce11a5ecd835c7ce7a2f6a0663
brennen <161400377+brennenxyz@users.noreply.github.com>
2024-04-28 22:19:16 -0400

    fire
    added randomized clicks
    
    added toggle

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
 let prefix = "§l§4[§l§cChocoClicker§l§4]§b "
-let delay = 75;
+
+let d = 75;
+let delayMultiplier = 1;
+
 let onDelay = false;
+let clicks = 0;
+let slowed = false;
+
+let enabled = false;
+let realClicks = 0;
+
+register("command", () => {
+    enabled = !enabled
+    ChatLib.chat(prefix + "Status: " + enabled)
+}).setName("chococlicker")
 
 register("command", (arg1) => {
     if(!arg1) return ChatLib.chat(prefix + "Invalid Syntax!");
     if(!parseInt(arg1)) return ChatLib.chat(prefix + "Invalid Syntax!");
-    else delay = parseInt(arg1)
-
-    ChatLib.chat(prefix + "New Delay: " + arg1 + "ms/click")
+    else d = parseInt(arg1)
+    ChatLib.chat(new Message(new TextComponent(prefix + "New Delay: " + arg1 + "ms/click")))
 }).setName("setchocodelay")
 
-register("tick", () => {
+register("step", () => {
+    if(!enabled) return;
     if(!Player?.getContainer().getName().toLowerCase().includes("chocolate factory")) return;
-    //if(!Player.getInventory().getItems().slice(0, 9).findIndex(a => a?.getName()?.includes("Treecapitator")))
     if(onDelay) return;
     onDelay = true;
+    
+    let clickme = Player.getContainer().getItems().findIndex(a => a?.getName()?.toLowerCase()?.includes("click me!"))
+    
+    if(clickme !== -1) {
+        Player.getContainer().click(clickme, false, ClickAction.ClickType.RIGHT)
+        return;
+    }
 
-    Player.getContainer().click(13, false, ClickAction.ClickType.MIDDLE)
+    Player.getContainer().click(13, false, ClickAction.ClickType.RIGHT)
+
+    clicks++
+    realClicks++
+
+    if(clicks > 500 && !slowed) {
+        ChatLib.chat(prefix + "Temporarily Slowing Clicks By 33%")
+        delayMultiplier = 1.33
+        slowed = true;
+        clicks = 0;
+    } else if(clicks > 500 && slowed) {
+        ChatLib.chat(prefix + "Reverting Temporary Slowed Clicks")
+        delayMultiplier = 1
+        slowed = false;
+        clicks = 0;
+    }
+
+    ChatLib.clearChat(6)
+    ChatLib.chat(new Message(new TextComponent(prefix + "Click Amount: " + realClicks)).setChatLineId(6))
+
     setTimeout(() => {
         onDelay = false;
-    }, delay);
+    }, d*delayMultiplier+(Math.floor(Math.random() * 10) + 1));
 })
